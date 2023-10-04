486773550b0a281b08921f7869c12276758ee4ab
Tashrif-alive <111632512+Tashrif-alive@users.noreply.github.com>
2023-10-04 23:36:43 +0600

    little changes

--- lib/student.dart
+++ lib/student.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class StudentID extends StatelessWidget {
+  const StudentID({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
